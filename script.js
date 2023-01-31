56e5dbbce265da2e1d4e5c382f36ce98a17808d8
Evgeny <epsilon1293@gmail.com>
2023-01-31 20:52:54 +0200

    Movie app

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const numberOfFilms = +prompt('How many films have You seen?', '');
+
+const personalMovieDB = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const 
+a = prompt('One of films You have seen?', ''),
+      b = prompt('How could You rate it?', ''),
+      c = prompt('One of films You have seen?', ''),
+      d = prompt('How could You rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
