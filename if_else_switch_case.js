29b5d7cf4da976876c56d138cbe52c93cd151cc6
Evgeny <epsilon1293@gmail.com>
2023-02-01 15:00:29 +0200

    if else switch changes

---
+++ if_else_switch_case.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// if (4 == '4') {
+//     console.log('OK');
+// }
+
+// else {
+//     console.log('ERROR');
+// }
+
+// const num = 50;
+
+// if ( num < 50) {
+//     console.log('ERROR');
+// }
+
+// else if (num > 100) {
+//     console.log('Many');
+// }
+// else {
+//     console.log('OK');
+// }
+
+const num = 52;
+
+// (num === 50) ? console.log('OK') : console.log('ERROR');  // TERNARNQJ OPERATOR
+
+switch (num) {
+    case 49:
+        console.log('FAULT');
+        break;
+    case 100:
+        console.log('FAULT');
+        break;
+    case 50:
+        console.log('OK');
+        break;
+    default:
+        console.log('Maybe later');
+        break;
+}
+
+// const num = '50';
+
+// if (num == 50) {
+
+//     console.log('OK');
+// }
+
+// else if (num < 50) {
+//     console.log('Less than you need');
+// }
+
+// else if (num > 50) {
+//     console.log('More than you need');
+// }
+
+// else {
+//     console.log('error');
+// }
+
+// if (4 == '4') {
+//     console.log('OK');
+// }
+
+// else {
+//     console.log('ERROR');
+// }
